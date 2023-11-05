32fbf82d422c033e65890d1f2157e98d5564cedc
srs-sieam <srssieam@gmail.com>
2023-11-05 07:34:58 +0600

    added router in main.jsx

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layouts/Root";
+import Home from "../Pages/Home";
+
+
+const Routes = createBrowserRouter([
+    {
+        path:'/',
+        element:<Root></Root>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
+    }
+])
+
+export default Routes;
